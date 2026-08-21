cf81185e89638dfb6c556f4878902f648729c1a7
Shipnurse <se.calzari@libero.it>
2026-08-22 01:50:37 +0200

    Update service worker to use new cache version

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,5 @@
-const CACHE_NAME = 'icu-cvc-v1.1';
-
-const ASSETS = [
+const CACHE_NAME = 'icu-cvc-v3';
+const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
@@ -8,56 +7,30 @@ const ASSETS = [
   './icon-512.png'
 ];
 
-// Installazione della nuova versione
 self.addEventListener('install', event => {
-  self.skipWaiting();
-
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-// Attivazione: elimina le vecchie cache
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
       );
-    }).then(() => self.clients.claim())
+    })
   );
+  self.clients.claim();
 });
 
-// Per index.html prova prima la rete.
-// In questo modo le nuove versioni vengono rilevate.
 self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy = response.clone();
-
-          caches.open(CACHE_NAME).then(cache => {
-            cache.put('./index.html', copy);
-          });
-
-          return response;
-        })
-        .catch(() => {
-          return caches.match('./index.html');
-        })
-    );
-
-    return;
-  }
-
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
